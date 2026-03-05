2c0ea726528db215ecd718273da0b750fe7a9225
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-03-05 20:46:42 +0630

    remove unnecessary comments from discountcheck.php

--- discountcheck.php
+++ discountcheck.php
@@ -1,8 +1,6 @@
 <?php
-// 1. Set the price variable
 $price = 120;
 
-// 2. Check if the price is eligible for a discount
 if ($price > 100) {
     $finalPrice = $price - 10;
     echo "You got a $10 discount! Final price: $" . $finalPrice;

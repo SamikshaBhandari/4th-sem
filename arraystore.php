cd7ec2fd92a73f3fafff6a455c180dd3dd4adb72
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-03-06 21:14:10 +0630

    add arraystore.php to display homestay names

---
+++ arraystore.php
@@ -0,0 +1,9 @@
+<?php
+// 1. Create an array of homestay names
+$homestays = ["Mountain View", "Lakeside Home", "Forest Cottage"];
+
+// 2. Use a foreach loop to print each item in the list
+foreach ($homestays as $name) {
+    echo "Homestay Name: " . $name . "<br>";
+}
+?>

468d3c88008f31539fc45f6bff73bc433d0e2a3b
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-03-08 17:14:06 +0545

    add switch.php to display the current day based on a number

---
+++ switch.php
@@ -0,0 +1,17 @@
+<?php
+$dayNumber = 3;
+
+switch ($dayNumber) {
+    case 1:
+        echo "Today is Monday";
+        break;
+    case 2:
+        echo "Today is Tuesday";
+        break;
+    case 3:
+        echo "Today is Wednesday";
+        break;
+    default:
+        echo "It's another day of the week";
+}
+?>

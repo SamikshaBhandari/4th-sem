398a9684bddf5124ce823d9d05e6b11b3f433978
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-03-12 07:39:53 +0545

    remove unnecessary comments from assignvalue.php

--- assignvalue.php
+++ assignvalue.php
@@ -1,11 +1,8 @@
 <?php
-// 1. Assign values to two variables
 $number1 = 20;
 $number2 = 30;
 
-// 2. Perform the addition
 $sum = $number1 + $number2;
 
-// 3. Display the result
 echo "The sum of $number1 and $number2 is: " . $sum;
 ?>

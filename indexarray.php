eb5ba6be35cd28bc21eeb8a14fff1f4db455cd89
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-03-13 19:30:11 +0545

    add indexarray.php to display skills and their count

---
+++ indexarray.php
@@ -0,0 +1,6 @@
+<?php
+$skills = ["PHP", "MySQL", "JavaScript", "React"];
+echo "First Skill: " . $skills[0] . "<br>"; 
+echo "Second Skill: " . $skills[1] . "<br>"; 
+echo "Total Skills: " . count($skills);
+?>

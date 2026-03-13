fa85f2ec0dcec92449607b3317bba3273f091f03
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-03-13 19:30:22 +0545

    add multidimensionalarray.php to display student subjects and marks

---
+++ multidimensionalarray.php
@@ -0,0 +1,10 @@
+<?php
+$students = [
+    ["Samiksha", "PHP", 95],
+    ["sagar", "DBMS", 88],
+    ["sanjib", "Networking", 82]
+];
+
+echo "Student: " . $students[0][0] . " | Subject: " . $students[0][1] . " | Marks: " . $students[0][2] . "<br>";
+echo "Student: " . $students[1][0] . " | Subject: " . $students[1][1] . " | Marks: " . $students[1][2];
+?>
